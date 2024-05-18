c05b9d674250a251e7e0116826997a963c3f0696
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-05-18 21:42:37 +0200

    added cardClassWrapper that wraps card struct and adds methods ==, <, so that it can be used in std::map

--- card_structs.h
+++ card_structs.h
@@ -1,26 +1,75 @@
 #ifndef CARD_STRUCTS_H
 #define CARD_STRUCTS_H
 
+#include <map>
 #include <cstdint>
 
-enum FigureCardValue {
+enum FigureCardValue
+{
     J = 11,
     Q,
     K,
     A
 };
 
-enum Suit {
+enum Suit
+{
     HEARTS = 'H',
     DIAMONDS = 'D',
     CLUBS = 'C',
     SPADES = 'S'
 };
+namespace deck
+{
+    typedef struct __attribute__((__packed__)) Card
+    {
+        Suit suit;
+        uint8_t value;
+    } Card;
 
-typedef struct __attribute__ ((__packed__)) Card {
-    Suit suit;
-    uint8_t value;
-} Card;
+    class CardClassWrapper
+    {
+    public:
+        // Constructors
+        CardClassWrapper() = delete;
+        CardClassWrapper(Suit suit, uint8_t value) : card({suit, value}) {}
+        CardClassWrapper(Card card) : card(card) {}
 
+        // Destructor
+        ~CardClassWrapper() = default;
+
+        // Operators
+        bool operator==(const CardClassWrapper &other) const
+        {
+            return card.suit == other.card.suit && card.value == other.card.value;
+        }
+
+        bool operator<(const CardClassWrapper &other) const
+        {
+            return card.suit < other.card.suit || (card.suit == other.card.suit && card.value < other.card.value);
+        }
+
+        Suit get_suit() const
+        {
+            return card.suit;
+        }
+
+        uint8_t get_value() const
+        {
+            return card.value;
+        }
+
+    private:
+        Card card;
+    };
+
+    class AllCards
+    {
+    public:
+        // std::map<Suit, std::map<uint8_t, bool>> was_card_played_map;
+        // albo
+        std::map<CardClassWrapper, bool> was_card_played_map;
+    };
+} // namespace deck
 
 #endif // COMMUNICATION_STRUCTS_H
