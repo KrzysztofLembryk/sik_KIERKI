daf45f559dceb21f07ab566c22abc0cc50a1df67
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-05-19 20:54:55 +0200

    added count points method to game classes

--- game_classes.cpp
+++ game_classes.cpp
@@ -41,6 +41,17 @@ uint8_t count_colors(Suit suit, uint8_t val, deck::Lewa &lewa)
 }
 
 
+/**
+ * @brief Counts points for given game type
+ * There are seven game types:
+ * - NO_LEWA - 1 point for getting a Lewa
+ * - NO_HEART - 1 point for each heart
+ * - NO_QUEEN - 5 points for each queen
+ * - NO_MISTER - 2 points for each king and jack
+ * - NO_KING_HEART - 18 points for king of hearts
+ * - NO_SEVEN_AND_LAST - 10 points for seventh and thirteenth lewa
+ * - BANDIT - sum of all above
+*/
 uint8_t PointCounter::count_points(deck::Lewa &lewa)
 {
     uint8_t points = 0;
@@ -56,16 +67,25 @@ uint8_t PointCounter::count_points(deck::Lewa &lewa)
             points = count_figures(Q, 5, lewa);
             break;
         case NO_MISTER:
-            // points = count_figures(K, 5, lewa);
+            points = count_figures(K, 2, lewa);
+            points += count_figures(J, 2, lewa);
             break;
         case NO_KING_HEART:
-            return 0;
+            points = count_figures(K, 18, lewa, true);
             break;
         case NO_SEVEN_AND_LAST:
-            return 0;
+            if (lewa.get_lewa_id() == 7 || lewa.get_lewa_id() == 13)
+                points = 10;
             break;
         case BANDIT:
-            return 0;
+            points = 1;
+            points += count_colors(HEARTS, 1, lewa);
+            points += count_figures(Q, 5, lewa);
+            points += count_figures(K, 2, lewa);
+            points += count_figures(J, 2, lewa);
+            points += count_figures(K, 18, lewa, true);
+            if (lewa.get_lewa_id() == 7 || lewa.get_lewa_id() == 13)
+                points += 10;
             break;
     }
     return points;
