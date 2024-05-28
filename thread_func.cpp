db01b03cc850639064d427de3b2a58bdc0fdb2b8
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-05-28 16:32:52 +0200

    sending END message to server works!!

--- thread_func.cpp
+++ thread_func.cpp
@@ -1,5 +1,5 @@
 #include "thread_func.h"
-
+#include <iostream>
 
 void thread_nmspc::MyThread::thread_main(
         std::shared_ptr<ClientFdWrapper> client_fd_sp,
@@ -7,5 +7,10 @@ void thread_nmspc::MyThread::thread_main(
         std::shared_ptr<Player> player_sp,
         int pipe_write_fd)
 {
-    
+    sleep(5);
+    std::cout << "thread sending END\n";
+    fflush(stdout);
+
+    char pipe_buffer[] {'E', 'N', 'D'}; 
+    write(pipe_write_fd, pipe_buffer, sizeof(pipe_buffer));
 }
